22bc765712133bbdbc6287fda4b7f23d986410f1
Fynn Leitow <19202953+LyteFM@users.noreply.github.com>
2021-04-12 22:25:38 +0200

    :construction_worker: Trying out CI with newer versions
    test against node 12, 14 and 16; use CouchDB 3.1

--- test/test.config.js
+++ test/test.config.js
@@ -8,8 +8,8 @@ module.exports = {
   dbServer: {
     protocol: process.env.COUCH_PROTOCOL || 'http://',
     host: process.env.COUCH_HOST || 'localhost:5984',
-    user: process.env.COUCH_USER || '',
-    password: process.env.COUCH_PASS || '',
+    user: process.env.COUCH_USER || 'admin',
+    password: process.env.COUCH_PASS || 'password',
     userDB: 'sl_test-users',
     couchAuthDB: 'sl_test-keys'
   },
